6a3ccbf6323bcabdc7a7124a452ca46199f4f60e
cristiannZzzz <cristian.bulla02@gmail.com>
2025-05-11 20:09:45 -0400

    feat: Remove Veterinarian Dashboard and related components to streamline application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AdminDashboard  from "./components/Dashboard/Admin/AdminDashboard";
+import AdminDashboard from "./components/Dashboard/Admin/AdminDashboard";
 import ClientDashboard from "./components/Dashboard/Cliente/ClientDashboard";
 import LoginForm from "./components/Login/LoginForm";
 import RegisterForm from "./components/Register/RegisterForm";
-import VeterinarianDashboard from "./components/Dashboard/Veterinarian/VeterinarianDashboard";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/client" element={<ClientDashboard />} />
-        <Route path="/veterinario" element={<VeterinarianDashboard />} />
       </Routes>
     </Router>
   );
